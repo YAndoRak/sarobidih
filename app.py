8b4fbaf59e918ff56154e2388813ee4fb00e1495
ANDRIANAVALONA R. Ando <rakotofiringa.ando.andrianavalona@esti.mg>
2020-07-19 22:15:50 +0300

    nanala fonction button

--- app.py
+++ app.py
@@ -49,7 +49,6 @@ def receive_message():
                         send_message(recipient_id, response_sent_text)
                 if message['message'].get('attachments'):
                     response_sent_nontext = get_message()
-                    send_BM(recipient_id, response_sent_text,elements2)
                     send_message(recipient_id, response_sent_nontext)
     return "Message Processed"
 
