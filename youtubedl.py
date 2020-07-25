0505d255a9a68a2753d619c8615842ae52ce4c24
ANDRIANAVALONA R. Ando <rakotofiringa.ando.andrianavalona@esti.mg>
2020-07-25 22:20:46 +0300

    m4a

--- youtubedl.py
+++ youtubedl.py
@@ -1,5 +1,5 @@
+from __future__ import unicode_literals
 import youtube_dl
-
 ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
 def find_ydl_url(url):
     with ydl:
@@ -25,29 +25,43 @@ def find_ydl_url(url):
             print('=================================== 360 P ====================================')
             return video_url
 def find_audio_url(url_audio):
-    ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
-    }
+    with ydl:
+        result = ydl.extract_info(
+            url_audio,
+            download=False # We just want to extract the info
+        )
 
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        result = ydl.extract_info(url_audio, download=False)
     if 'entries' in result:
-        audios = result['entries'][0]
+        video = result['entries'][0]
     else:
-        audios = result
-    audios['formats']
-    for audio in audios:
-        print('=================================== 360 P ====================================')
-        print('Extension : {}'.format(audio['ext']))
-        print('URL : {}'.format(audio['url']))
-        print('Fomart ID: {}'.format(audio['format_id']))
-        print('Fomart : {}'.format(audio['format']))
-        print('Filesize : {}'.format(audio['filesize']))
-        print('=================================== 360 P ====================================')
-    print(audios['formats'])
-    return audios['formats'][0]['url']
+        video = result
+
+    video_urls = video['formats']
+    for video_url in video_urls:
+        if video_url['ext'] == 'm4a' :
+            print('=================================== 360 P ====================================')
+            print('Extension : {}'.format(video_url['ext']))
+            print('URL : {}'.format(video_url['url']))
+            print('Fomart ID: {}'.format(video_url['format_id']))
+            print('Fomart : {}'.format(video_url['format']))
+            print('Filesize : {}'.format(video_url['filesize']))
+            print('=================================== 360 P ====================================')
+            return video_url
+#     ydl_opts = {
+#     'format': 'bestaudio/best',
+#     'postprocessors': [{
+#         'key': 'FFmpegExtractAudio',
+#         'preferredcodec': 'mp3',
+#         'preferredquality': '192'
+#     }],
+#     'postprocessor_args': [
+#         '-ar', '16000'
+#     ],
+#     'prefer_ffmpeg': True,
+#     'keepvideo': True
+#     }
+#     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#         ydl.download(url_audio)
+
+# if __name__ == "__main__":
+#     print(find_audio_url(["https://www.youtube.com/watch?v=Cfv7qHMeNS4"]))
