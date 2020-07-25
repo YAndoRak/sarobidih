94bb37da4572cacd6b6f94a78a03f632d14d7af7
ANDRIANAVALONA R. Ando <rakotofiringa.ando.andrianavalona@esti.mg>
2020-07-25 16:03:39 +0300

    upload audio response

--- app.py
+++ app.py
@@ -217,8 +217,8 @@ def upload_audio_fb(recipient_id):
     videme = requests.post("https://graph.facebook.com/v7.0/me/message_attachments",
     params={"access_token": ACCESS_TOKEN},
     data=payload, headers = {"Content-Type": "application/json"})
-    print(videme.Response())
-    app.logger.debug(videme.Response())
+    print(videme.content)
+    app.logger.debug(videme.content)
     #upload_audio_attachements(recipient_id, videme.Response()['message'].get('attachment_id'))
 # def send_message_video(recipien_id, response):
 #     bot.send_video(recipien_id, response)
