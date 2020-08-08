0ee14cff4eb8fe0fb063ac6b4a4b902ba5b3d147
ANDRIAMIALY Sarobidy <sarobidy.antonio.andriamialy@esti.mg>
2020-08-08 12:00:47 +0300

    modify help message

--- response.py
+++ response.py
@@ -1,8 +1,7 @@
 def help():
     help = ('Help : Bienvenue sur le HooLLa BoT 🤩👋\n'
-          'Ici vous pouvez faire des recherche sur google et youtube en utilisant les syntaxes suivantes 🤳:\n\n'
-          '\t🔑 Recherche par youtube \t==> \t ytb <video rechercher> 🎞️\n'
-          '\t🔑 Recherche par google \t==> \t gg <mot cle  à rechercher> 🔍\n\n'
+          'Ici vous pouvez faire des recherche youtube en utilisant les syntaxes suivantes 🤳:\n\n'
+          '🔑 Recherche par youtube \t==> \t ytb <video rechercher> 🎞️\n (exemple: ytb zezika)'
           "Si malgrès tous cela ne marche pas 😧 , je vous invite à envoyer un message aux responsables directement par MP 😎\n"
           "J'espère vous être utile. 👨‍🔧\n"
           "Profiter au maximum 💯")
