f80989811e16334b5c7efca45184f802dfb4af38
ANDRIAMIALY Sarobidy <sarobidy.antonio.andriamialy@esti.mg>
2020-08-10 07:47:41 +0300

    add app

--- app.py
+++ app.py
@@ -546,7 +546,6 @@ def send_generic_template_youtube(recipient_id, research_query):
 
     payload = []
     for result in results['search_result']:
-        ydl = find_ydl_url(result["link"])
         payload.append({
             "title": result["title"],
             "image_url": result['thumbnails'][2],
