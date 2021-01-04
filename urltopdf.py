4f76f03fe23d89fa9e9a5836098d538759f1f0b7
Ando ANDRIANAVALONA <rakotofiringa.ando.andrianavalona@esti.mg>
2021-01-04 18:36:47 +0300

    Rename test

--- urltopdf.py
+++ urltopdf.py
@@ -2,15 +2,15 @@ import requests
 def convert_url_img(urls):
 	url="https://webpagetopdf999.herokuapp.com/api/render?output=screenshot&url="+urls
 	response = requests.get(url)
-	path = "tmp/render.png"
-	with open('tmp/render.png', 'wb') as f:
+	path = "tmp/google.png"
+	with open('tmp/google.png', 'a') as f:
 		f.write(response.content)
 	return path
 
 def convert_url_pdf(urls):
 	url="https://webpagetopdf999.herokuapp.com/api/render?url="+urls+"&scrollPage=true"
 	response = requests.get(url)
-	path = "tmp/render.pdf"
-	with open('tmp/render.pdf', 'wb') as f:
+	path = "tmp/google.pdf"
+	with open('tmp/google.pdf', 'a') as f:
 		f.write(response.content)
 	return path
