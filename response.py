e1c2050533c973a22ffd462f85c1228192547499
Ando ANDRIANAVALONA <rakotofiringa.ando.andrianavalona@esti.mg>
2021-04-26 15:56:43 +0300

    mise a jour reponse aide

--- response.py
+++ response.py
@@ -1,13 +1,14 @@
 def help():
     help = ('Hello 🤩👋\n\n'
           '🔑 Recherche par youtube ==> ytb <video rechercher> (exemple: ytb zezika) \n\n'
+          '🔑 Recherche sur google ==> gg <mot cle> (exemple: gg zavatra) \n\n'
           "J'espère vous être utile. 👨‍🔧")
     return help
 
 def other():
     other = ("🤩👋 Bienvenue sur le HooLLa BoT 🤩👋\n"
              "On est heureux de pouvoir vous aider,\n\n"
-             "entrer le message 🆘'HELP'🆘 si vous avez besoin d'aide")
+             "envoyer le message 🆘'HELP'🆘 si vous avez besoin d'aide")
     return other
 
 
